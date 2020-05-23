e91930718534545da1f7673ad44c85a25538d375
Andrew Johnson <blackandred@users.noreply.github.com>
2020-05-23 12:44:00 +0200

    Add tests coverage

---
+++ test/test_standardlib_python_publish.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import unittest
+import subprocess
+
+
+class PublishTaskTest(unittest.TestCase):
+    def _call_publish_via_shell(self, arguments: str):
+        try:
+            out = subprocess.check_output('''
+                export PYTHONPATH=$(pwd)
+                cd ..
+                python3 -m rkd --silent :sh -c "python3 -m rkd --silent :py:publish ''' + arguments + '''"
+            ''', shell=True)
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+        return out
+
+    def test_integration_is_output_properly_catch_by_sh(self):
+        """Case: In the past there were problems with output displaying by RKD-in-RKD - sh('rkd :py:publish ...')
+
+        Integration test with TaskUtilities.sh()
+        """
+
+        out = self._call_publish_via_shell('--username=wrong --password=wrong --test').decode('utf-8')
+        self.assertIn('403 Invalid or non-existent authentication information.', out)
+        self.assertIn('Uploading distributions to https://test.pypi.org', out)
