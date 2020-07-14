6908ea1e3ff4f16da143b82d929307e94114934c
Andrew Johnson <blackandred@users.noreply.github.com>
2020-07-14 07:04:04 +0200

    #26: Additional coverage

--- test/test_standardlib_createstructure.py
+++ test/test_standardlib_createstructure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 import unittest
 from tempfile import TemporaryDirectory
 from rkd.standardlib import CreateStructureTask
@@ -20,6 +21,8 @@ class CreateStructureTaskTest(unittest.TestCase):
         return io
 
     def test_functional_creates_structure_in_temporary_directory(self):
+        """Test a successful case"""
+
         with TemporaryDirectory() as tempdir:
             cwd = os.getcwd()
 
@@ -36,6 +39,53 @@ class CreateStructureTaskTest(unittest.TestCase):
                                 msg='Expected requirements.txt file to be present')
                 self.assertTrue(os.path.isfile(tempdir + '/.venv/bin/activate'),
                                 msg='Expected that virtual environment will contain a bin/activate file')
+
+            finally:
+                os.chdir(cwd)
+
+    def test_functional_detects_git_is_dirty(self):
+        """Verify that GIT workspace is unclean - there are pending working changes not commited"""
+
+        with TemporaryDirectory() as tempdir:
+            cwd = os.getcwd()
+
+            try:
+                os.chdir(tempdir)
+
+                # ---------------------------
+                # Prepare the workspace first
+                # ---------------------------
+                subprocess.call(['git', 'init'])
+                subprocess.call('echo "test-1" > test-file.txt; git add test-file.txt', shell=True)
+                subprocess.call('git commit -m "First commit"', shell=True)
+                # make the working tree dirty by editing a file without a commit
+                subprocess.call('echo "changed" > test-file.txt', shell=True)
+
+                io = self._execute_mocked_task({
+                    '--commit': True,
+                    '--no-venv': False
+                }, {})
+
+                self.assertIn('Current working directory is dirty', io.get_value())
+            finally:
+                os.chdir(cwd)
+
+    def test_functional_no_virtualenv_environment_is_created_when_a_switch_was_used(self):
+        """Verify that --no-venv switch would end with skipped virtual environment creation"""
+
+        with TemporaryDirectory() as tempdir:
+            cwd = os.getcwd()
+
+            try:
+                os.chdir(tempdir)
+
+                # action
+                self._execute_mocked_task({'--commit': False, '--no-venv': True}, {})
+
+                # assertions
+                self.assertTrue(os.path.isfile(tempdir + '/requirements.txt'),
+                                msg='Expected requirements.txt file to be present')
+                self.assertFalse(os.path.isfile(tempdir + '/.venv/bin/activate'))
 
             finally:
                 os.chdir(cwd)
