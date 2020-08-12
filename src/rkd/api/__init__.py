6fd727254771d9e5f324afd7832df2e462e22a36
Andrew Johnson <blackandred@users.noreply.github.com>
2020-08-12 08:35:28 +0200

    #39: Directory structure refactor

---
+++ src/rkd/api/__init__.py
@@ -0,0 +1,8 @@
+"""
+    RKD API
+    =======
+
+    Contracts published with each RKD version. All changes there needs to be done with careful to not break
+    any tasks developed by other non-maintainer people.
+
+"""
---
+++ src/rkd/execution/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Tasks Executor
+    ==============
+
+    Responsible for executing tasks code, serialization, process separation, privileges management, passing the
+    already prepared context, processing the results, processing declarative code
+
+    Responsibilities are split into separate sub-layers.
+
+"""
