018b78b146df87353328ed6b8902c452b2cce5f8
Andrew Johnson <blackandred@users.noreply.github.com>
2020-11-27 09:03:43 +0100

    #54: Corrections to the package_data to include "misc" directory

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     pbr=True,
     package_dir={'': './'},
     packages=find_packages(where='./'),
-    include_package_data=True
+    include_package_data=True,
+    package_data={
+        'rkd': ['misc/*', 'misc/**/*', 'misc/initial-structure/.rkd/*', 'misc/internal/**/*']
+    }
 )
