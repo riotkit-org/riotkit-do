526e6d60f8ac3864c1f26b80db50132a506d6176
Webbie <dkwebbie@users.noreply.github.com>
2021-01-17 05:34:57 +0100

    [#50] Working implementation of @rescue, @error and @retry modifiers

--- rkd/argparsing/model.py
+++ rkd/argparsing/model.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import List
+from typing import List, Dict
 
 
 class TaskArguments(object):
@@ -27,21 +27,51 @@ class TaskArguments(object):
 
 
 class ArgumentBlock(object):
-    body: str
-    on_rescue: str = ''
-    on_error: str = ''
+    body: List[str]
+    on_rescue: List[TaskArguments]
+    on_error: List[TaskArguments]
     retry: int = 0
-    _tasks: List[TaskArguments]
 
-    def __init__(self, body: str = '', rescue: str = '', error: str = '', retry: int = 0):
+    _tasks: List[TaskArguments]
+    _raw_attributes: dict
+    _retry_counter: Dict['TaskDeclaration', int]
+
+    def __init__(self, body: List[str] = None, rescue: str = '', error: str = '', retry: int = 0):
         """
         :param body Can be empty - it means that block will have tasks filled up later
         """
 
+        if body is None:
+            body = []
+
         self.body = body
-        self.on_rescue = rescue
-        self.on_error = error
-        self.retry = retry
+        try:
+            self.retry = int(retry)
+        except ValueError:
+            self.retry = 0
+
+        self.on_rescue = []
+        self.on_error = []
+        self._retry_counter = {}
+
+        # those attributes will be lazy-parsed on later processing stage
+        self._raw_attributes = {
+            'rescue': rescue,
+            'error': error,
+        }
+
+    @staticmethod
+    def from_empty() -> 'ArgumentBlock':
+        """Dummy instance"""
+
+        instance = ArgumentBlock(
+            body=[], rescue='', error='', retry=0
+        )
+
+        instance.set_parsed_rescue([])
+        instance.set_parsed_error_handler([])
+
+        return instance
 
     def with_tasks(self, tasks_arguments: List[TaskArguments]):
         cloned = deepcopy(self)
@@ -57,3 +87,41 @@ class ArgumentBlock(object):
             return self.with_tasks(blocks[0]._tasks)
         except IndexError:
             return self
+
+    def raw_attributes(self) -> dict:
+        return self._raw_attributes
+
+    def set_parsed_error_handler(self, tasks_arguments: List[TaskArguments]) -> None:
+        self.on_error = tasks_arguments
+
+    def set_parsed_rescue(self, tasks_arguments: List[TaskArguments]) -> None:
+        self.on_rescue = tasks_arguments
+
+    def should_task_be_retried(self, declaration):
+        # no retry available at all
+        if self.retry < 1:
+            return False
+
+        # has to retry, but it is a first time
+        if declaration not in self._retry_counter:
+            return True
+
+        return self._retry_counter[declaration] < self.retry
+
+    def task_retried(self, declaration):
+        """Takes notification from external source to internally note that given task was retried"""
+
+        if declaration not in self._retry_counter:
+            self._retry_counter[declaration] = 0
+
+        self._retry_counter[declaration] += 1
+
+    def should_rescue(self):
+        """Decides if given task should have executed a rescue set of tasks"""
+
+        return len(self.on_rescue) > 0
+
+    def has_action_on_error(self):
+        """Asks if there is a set of tasks that should be notified on error"""
+
+        return len(self.on_error) > 0
