19d3ba0b34d84646e82acd8b986e949d75497921
Andrew Johnson <blackandred@users.noreply.github.com>
2021-06-12 11:41:28 +0200

    #74: Fix deps

--- src/core/setup.py
+++ src/core/setup.py
@@ -3,7 +3,6 @@
 from riotkit.pbs import get_setup_attributes
 from setuptools import setup, find_namespace_packages
 
-
 setup(
     **get_setup_attributes(root_dir='./', git_root_dir='../../'),
     packages=find_namespace_packages(include='rkd.*', exclude=('tests',)),
